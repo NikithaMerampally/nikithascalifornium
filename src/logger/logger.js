2c11942ab70902aca72aaddecb963b38f543f3fd
NikithaMerampally <merampallynikitha@gmail.com>
2022-12-08 20:15:12 +0530

    use commit

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+const myfunction=function welcome(name){
+    console.log(`Welcome to my application. I am  and a ${name} part of FunctionUp Californium cohort`)
+        
+}
+
+module.exports.myfunction=myfunction;

91806988a0bd2a0f8ffe4295f554a12a1713ac33
NikithaMerampally <merampallynikitha@gmail.com>
2022-12-08 17:22:19 +0530

    added external commands

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const intro=require('./intro')
+const module2=require(`./functionmodule`)
 
 router.get('/test-me', function (req, res) {
     res.send('any dummy text')
+    console.log(`my email is`,intro.myEmail)
+    console.log(`my function is`,module2.myFunction('nikitha'))
 });
 
 

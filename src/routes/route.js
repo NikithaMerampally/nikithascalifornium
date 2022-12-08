d3fcc0868179aa1fa937e6af4e2c84a459b24cfd
NikithaMerampally <merampallynikitha@gmail.com>
2022-12-08 13:54:13 +0530

    add changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My first third api!')
 });
+router.get('/test-you',function(req,res){
+    res.send("hiii hello i have changed to test-you")
+
+})
 
 module.exports = router;
